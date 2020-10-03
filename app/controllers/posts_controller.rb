26a31c0aaa39db431a3606654f732c9339d8c59d
Flavio Mahoney <flavio@mahoneyt.com>
2020-10-03 19:56:26 -0300

    Added index, show and create to the posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,24 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post  = Post.new(post_params)
+		@post.save
+		redirect_to index_path
+	end
+
+	private
+	def post_params
+		params.require(:post).permit(:title, :date, :description)
+	end
+
 end
