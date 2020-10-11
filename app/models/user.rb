98947df949126b6c787d005b5301d0be949542ed
Flavio Mahoney <flavio@mahoneyt.com>
2020-10-10 22:13:33 -0300

    Added dependencies on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :posts, through: :status
+  has_many :status, dependent: :destroy
 end
